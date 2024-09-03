c49cc19ef9c1ec1cbb09e8de96d72feb55d8ca9a
Sujan Pokharel <sujan.pokharel@dreambroker.fi>
2024-09-03 11:00:17 +0300

    maven related configurations

---
+++ src/test/java/PortfolioApiCall/PortfolioApiCallTests.java
@@ -0,0 +1,13 @@
+package PortfolioApiCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiCallTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
