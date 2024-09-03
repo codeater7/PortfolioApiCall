b5cee83fe5b8cd9bffcb42c8e30cf85dd299d908
Sujan Pokharel <sujan.pokharel@dreambroker.fi>
2024-09-03 11:12:23 +0300

    Springboot application main class

---
+++ src/main/java/PortfolioApiCall/PortfolioApiCall.java
@@ -0,0 +1,16 @@
+package PortfolioApiCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+//@EnableConfigurationProperties
+
+public class PortfolioApiCall {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApiCall.class, args);
+	}
+
+}
